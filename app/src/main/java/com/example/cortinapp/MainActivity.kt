1c5c31c9c76058c11e9b002f0315b83b51335ad7
JoseArias5541 <90353203+JoseArias5541@users.noreply.github.com>
2021-11-16 10:30:04 -0500

    First Commit

---
+++ app/src/main/java/com/example/cortinapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.cortinapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main_activity, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+}
